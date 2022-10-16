f2c9b3cc8a4cb75b5b8a8aae2d8eeb95b0ca8d2f
Anthony Nardi <anthonygt@gmail.com>
2022-10-15 22:57:21 -0400

    use port set on env

--- workspaces/server/src/main.ts
+++ workspaces/server/src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap()
   app.setGlobalPrefix('api');
   app.enableShutdownHooks();
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
